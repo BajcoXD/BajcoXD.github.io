13c38d32a58b06ba75d6bb3246c2df8f5ac7916a
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2024-12-11 02:13:53 +0100

    w

---
+++ Homeworks/JavaScript-Basic/JsHomework03/script.js
@@ -0,0 +1,16 @@
+function years() {
+  let age = Number(prompt("Enter age: "));
+  let conversion = Number(prompt("1-human to dog or 2-dog to human"));
+
+if (conversion === 1) {
+  return age * 7;
+} else if (conversion === 2) {
+  return age / 7;
+} else {
+    return "Invailed input"
+}
+
+}
+
+sum = years();
+alert(`The age is ${sum}`);
