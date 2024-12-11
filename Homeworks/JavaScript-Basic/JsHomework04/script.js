a853dd9cff9ac7ed20a5b2642c74936117f775a2
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2024-12-11 02:57:41 +0100

    w

--- Homeworks/JavaScript-Basic/JsHomework04/script.js
+++ Homeworks/JavaScript-Basic/JsHomework04/script.js
@@ -2,6 +2,9 @@ let balance = 15000;
 let amount = Number(prompt(`Your balance is ${balance}, enter the withdraw amount:`));
 
 function atm() {
+  if (amount < 0){
+    return `The amount you input ${amount} is invalid`
+  }
   if (amount <= balance) {
     balance -= amount;
     return `You withdrew ${amount}, and you have ${balance} remaning`;
