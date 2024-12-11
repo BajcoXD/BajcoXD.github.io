0d2b0de7d34b758399edfc6613b042e2fe09b2a7
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2024-12-11 01:41:11 +0100

    w

---
+++ Homeworks/JavaScript-Basic/JsHomework02/script.js
@@ -0,0 +1,10 @@
+function getType(input) {
+    let type = typeof input;
+    console.log(`The tpy is: ${type}`);
+    return type;
+}
+getType("something")
+getType(2)
+getType(false)
+getType({})
+getType()
