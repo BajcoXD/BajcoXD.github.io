db2c7f692586f6c2c4a580519affb703197c88cf
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2024-12-15 19:15:36 +0100

    w

---
+++ Homeworks/JavaScript-Basic/JsHomework07/script.js
@@ -0,0 +1,8 @@
+let array = ['Hello', 'there', 'my', 'name', 'is', 'Viktor']
+let firstWord = array[0]
+let secondWord = array[1]
+let thirdWord = array[2]
+let fourthWord = array[3]
+let fivethWord = array[4]
+let sixthWord = array[5]
+console.log(firstWord, secondWord, thirdWord, fourthWord, fivethWord, sixthWord);
