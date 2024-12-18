da468af7b299e5b235bc64cae1c149ebc87e87fb
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2024-12-18 19:49:34 +0100

    w

---
+++ Homeworks/JavaScript-Basic/JsHomework12/script.js
@@ -0,0 +1,17 @@
+let book = {
+  bookOneTitle: "Twenty Thousand Leagues Under the Sea",
+  bookOneAuthor: "Jules Verne",
+  bookOneReadingStatus: "",
+  bookTwoTitle: "Around the World in Eighty Days",
+  bookTwoAuther: "Jules Verne",
+  bookTwoReadingStatus: "",
+  bookRead: function read() {
+    if ((this.bookOneReadingStatus === "yes")) {
+      return `Then you have to read ${this.bookTwoTitle}`;
+    } else if((this.bookOneReadingStatus === "no")) {
+      return `${this.bookOneTitle} is a great book read it all the way`;
+    }
+  },
+};
+book.bookOneReadingStatus = prompt(`Have you read ${book.bookOneTitle}`);
+alert(book.bookRead());
