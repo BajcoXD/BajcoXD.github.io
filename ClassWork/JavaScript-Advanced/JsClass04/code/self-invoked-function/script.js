c2d9ca6bc225870d648db15eb3650d5970ba9595
Code <code@sedc.mk>
2025-01-23 19:16:09 +0100

    Self invoked functions

---
+++ ClassWork/JavaScript-Advanced/JsClass04/code/self-invoked-function/script.js
@@ -0,0 +1,23 @@
+console.log("============ SELF-INVOKED FUNCTIONS ========");
+
+(function () {
+  console.log("This will be executed immediately!");
+})();
+
+((num1, num2) => console.log(num1 + num2))(20, 25);
+
+// https://dummyjson.com/products/1
+
+() => {
+  const productUrl = "https://dummyjson.com/products/1";
+  const productContainer = document.getElementById("product-container");
+  fetch(productUrl)
+    .then((response) => response.json())
+    .then((product) => {
+      console.log(product);
+      productContainer.innerHTML = `
+    <h3>${product.title}</h3>
+    <p>${product.description}</p>
+    <p>${product.price}</p>`;
+    });
+};
