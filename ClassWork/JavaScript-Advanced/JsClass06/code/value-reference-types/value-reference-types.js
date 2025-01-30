39c152e6058f2393d26a373046949e22e1bb5364
Code <code@sedc.mk>
2025-01-30 18:11:45 +0100

    Value & Reference Types

--- ClassWork/JavaScript-Advanced/JsClass06/code/value-reference-types/value-reference-types.js
+++ ClassWork/JavaScript-Advanced/JsClass06/code/value-reference-types/value-reference-types.js
@@ -9,7 +9,6 @@ newValueTypeVariable = "This is a NEW string";
 console.log(valueTypeVariable); // "This is a string"
 console.log(newValueTypeVariable); // "This is a NEW string"
 
-
 console.log("");
 console.log("=============== REFERENCE TYPE ===============");
 // complex datatypes (objects, arrays, functions etc..)
@@ -18,17 +17,17 @@ console.log("=============== REFERENCE TYPE ===============");
 
 // ===> Example with object
 let personObject = {
-    id: 1,
-    firstName: "Bob",
-    lastName: "Bobsky",
-    printFullName: function () {
-        console.log(`${this.firstName} ${this.lastName}`)
-    }
-}
+  id: 1,
+  firstName: "Bob",
+  lastName: "Bobsky",
+  printFullName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+};
 
 let newPersonObject = personObject;
-personObject.firstName = "Pink"
-newPersonObject.lastName = "Panther"
+personObject.firstName = "Pink";
+newPersonObject.lastName = "Panther";
 
 personObject.printFullName(); // "Pink Panther"
 newPersonObject.printFullName(); // "Pink Panther"
@@ -43,4 +42,36 @@ newTestArray[0] = 999_999;
 console.log(testArray[0]);
 console.log(newTestArray[0]);
 
+console.log("");
+console.log("================= Creating copy of an Array =============");
 
+let numbersArray = [1, 2, 3, 4, 5];
+
+function copyArray(array) {
+  let result = [];
+  array.forEach((item) => result.push(item));
+  return result;
+}
+
+let numbersArrayCopy = copyArray(numbersArray);
+numbersArrayCopy.pop();
+console.log("The slice copy", numbersArrayCopy);
+console.log("The original", numbersArray);
+
+// Using slice()
+
+let numbersArraySliceCopy = numbersArray.slice();
+numbersArraySliceCopy.push(123, 123, 123);
+console.log("The slic copy", numbersArraySliceCopy);
+console.log("The original", numbersArray);
+
+// Using map()
+let numbersArrayMapCopy = numbersArray.map((n) => n);
+numbersArrayMapCopy.pop();
+console.log(numbersArrayMapCopy);
+
+// Using spread operator
+let numbersArraySpreadCopy = [...numbersArray];
+numbersArraySpreadCopy.shift();
+console.log("The spread copy", numbersArraySpreadCopy);
+console.log("The original", numbersArray);
