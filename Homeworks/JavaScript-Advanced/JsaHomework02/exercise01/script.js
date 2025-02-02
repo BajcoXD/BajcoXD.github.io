df2839e854a94f9c99f09381b6bd76332b38fc7d
Viktor Stojanovski <viktor.gaming2018@gmail.com>
2025-02-03 00:33:23 +0100

    Star Wars Planets Exercise

--- Homeworks/JavaScript-Advanced/JsaHomework02/exercise01/script.js
+++ Homeworks/JavaScript-Advanced/JsaHomework02/exercise01/script.js
@@ -1,19 +1,54 @@
-function fetchPlanets(url) {
-  fetch(url)
+let currentPage = 1;
+
+function fetchPlanets(page) {
+  fetch(`https://swapi.py4e.com/api/planets/?page=${page}`)
     .then((response) => response.json())
-    .then((data) => displayPlanets(data.results));
+    .then((data) => {
+      displayPlanets(data.results);
+      toggleButtons();
+    });
 }
 
 function displayPlanets(planets) {
-  const tableBody = document.getElementById("planetTableBody");
-  tableBody.innerHTML = "";
-  planets.slice(0, 10).forEach((planet) => {
-    let row = `<tr>
-            <td>${planet.name}</td>
-            <td>${planet.population}</td>
-            <td>${planet.climate}</td>
-            <td>${planet.gravity}</td>
-        </tr>`;
-    tableBody.innerHTML += row;
-  });
+  document.getElementById("planetTableBody").innerHTML = planets
+    .map(
+      (planet) => `<tr>
+        <td>${planet.name}</td>
+        <td>${planet.population}</td>
+        <td>${planet.climate}</td>
+        <td>${planet.gravity}</td>
+      </tr>`
+    )
+    .join("");
+}
+
+function toggleButtons() {
+  let nextButton = document.getElementById("nextButton");
+  let prevButton = document.getElementById("prevButton");
+
+  if (!nextButton) {
+    nextButton = document.createElement("button");
+    nextButton.id = "nextButton";
+    nextButton.textContent = "NEXT 10";
+    nextButton.onclick = () => {
+      currentPage = 2;
+      fetchPlanets(currentPage);
+    };
+    document.body.appendChild(nextButton);
+  }
+
+  if (!prevButton) {
+    prevButton = document.createElement("button");
+    prevButton.id = "prevButton";
+    prevButton.textContent = "PREVIOUS 10";
+    prevButton.onclick = () => {
+      currentPage = 1;
+      fetchPlanets(currentPage);
+    };
+    prevButton.style.display = "none";
+    document.body.appendChild(prevButton);
+  }
+
+  nextButton.style.display = currentPage === 1 ? "block" : "none";
+  prevButton.style.display = currentPage === 2 ? "block" : "none";
 }
