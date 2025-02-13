ba18150bc67fcccf90065895fa764cfdf0ab90bf
Code <code@sedc.mk>
2025-02-13 17:40:47 +0100

    Destructuring

--- ClassWork/JavaScript-Advanced/JsClass11/destructuring/script.js
+++ ClassWork/JavaScript-Advanced/JsClass11/destructuring/script.js
@@ -1,6 +1,8 @@
-console.log("====================== DESTRUCTURING ================");
+console.log("====================== DESTRUCTURING ======================");
+// => breaking down a complex structure into simpler parts
+// => cleaner syntax to get items from arrays, or property-values from objects, and put them into distinct variables
 
-let userOBJ = {
+let userObj = {
   id: 1,
   firstName: "Emily",
   lastName: "Johnson",
@@ -34,78 +36,198 @@ let userOBJ = {
     },
     country: "United States",
   },
-  macAddress: "47:fa:41:18:ec:eb",
-  university: "University of Wisconsin--Madison",
-  bank: {
-    cardExpire: "03/26",
-    cardNumber: "9289760655481815",
-    cardType: "Elo",
-    currency: "CNY",
-    iban: "YPUXISOBI7TTHPK2BR3HAIXL",
-  },
-  company: {
-    department: "Engineering",
-    name: "Dooley, Kozey and Cronin",
-    title: "Sales Manager",
-    address: {
-      address: "263 Tenth Street",
-      city: "San Francisco",
-      state: "Wisconsin",
-      stateCode: "WI",
-      postalCode: "37657",
-      coordinates: {
-        lat: 71.814525,
-        lng: -161.150263,
-      },
-      country: "United States",
-    },
-  },
-  ein: "977-175",
-  ssn: "900-590-289",
-  userAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
-  crypto: {
-    coin: "Bitcoin",
-    wallet: "0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a",
-    network: "Ethereum (ERC20)",
-  },
-  role: "admin",
 };
 
 console.log("");
-console.log("============ Object Destructuring ===========");
+console.log("============== Object Destructuring ==============");
 
-// =====> Example: take id, firstname, lastname
-// const id = userOBJ.id;
-// const firstName = userOBJ.firstName;
-// const lastName = userOBJ.lastName;
+// ===> Example: take id, firstName, lastName (WITHOUT DESTRUCTURING)
+// const id = userObj.id;
+// const firstName = userObj.firstName;
+// const lastName = userObj.lastName;
 // console.log(id, firstName, lastName);
 
-// ====> Example: take id, firstname , lastname, (WITH DESTRUCTURING)
-const { id, firstName, lastName } = userOBJ;
-console.log(id, firstName, lastName);
+// ===> Example: take id, firstName, lastName (WITH DESTRUCTURING)
+let { id, firstName, lastName } = userObj;
 // id = 10;
 console.log(id, firstName, lastName);
 
-// ====> Example: take id, firstname , lastname, (WITH DESTRUCTURING)
-const { userFirstName, userLastName, age } = userOBJ;
+// ===> Example: take firstName, lastName, age (WITH DESTRUCTURING)
+const { userFirstName, userLastName, age } = userObj;
 console.log(userFirstName, userLastName, age);
+// NOTE: We must use the same key-names (property names) !!!
 
-// ====> Example: take addres latitude and longitude (WITHOUT DESTRUCCTURING)
+// *However, there is workaround syntax even for that (BONUS)*
+// Syntax: { propertyName : ourCustomVariableName } = ourObject
+const { firstName: userFirstNameCustom } = userObj;
+console.log(userFirstNameCustom);
 
-let latitude = userOBJ.address.coordinates.lat;
-let longitude = userOBJ.address.coordinates.lng;
+// ===> Example: take address latitude and longitude (WITHOUT DESTRUCTURING)
+let latitude = userObj.address.coordinates.lat;
+let longitude = userObj.address.coordinates.lng;
 console.log(latitude, longitude);
 
-// ====> Example: take addres latitude and longitude (WITHOUT DESTRUCCTURING)
+// ===> Example: take address latitude and longitude (WITH DESTRUCTURING)
+// const { lng, lat } = userObj.address.coordinates;
+// console.log(lat, lng);
 
+// ===> Example: take address city, latitude and longitude (WITH DESTRUCTURING)
+const {
+  city,
+  coordinates: { lat, lng },
+} = userObj.address;
+console.log(city, lat, lng);
+
+console.log("");
+console.log("============== Parameters Destructuring ==============");
+
+// ===> Example: without destructuring
 function getUserInfo(user) {
   return `${user.firstName} ${user.lastName}`;
 }
 
-console.log(getUserInfo(userOBJ));
+console.log(getUserInfo(userObj));
 
+// ===> Example: with parameters destructuring
 function getUserInfoDest({ firstName, lastName, age }) {
   return `${firstName} ${lastName} ${age}`;
 }
-console.log(getUserInfoDest(userOBJ));
+console.log(getUserInfoDest(userObj));
+
+console.log("");
+console.log("============== Array Destructuring ==============");
+
+const testArray = [100, 300, 5_000, 400, 10_000];
+
+// ===> Example: take first 3 items from the array (WITHOUT DESCTRUCTURING)
+let firstNumber = testArray[0];
+let secondNumber = testArray[1];
+let thirdNumber = testArray[2];
+console.log(firstNumber, secondNumber, thirdNumber);
+
+// ===> Example: take first 3 items from the array (WITH DESCTRUCTURING)
+let [itemOne, itemTwo, itemThree, itemFive] = testArray;
+console.log(itemOne, itemTwo, itemThree, itemFive); //itemFive = 400
+
+// ===> Example: take first and third div with class array-demo (WITH DESCTRUCTURING)
+let [firstDiv, _, thirdDiv] = document.getElementsByClassName("array-demo");
+console.log(firstDiv);
+console.log(thirdDiv);
+
+// ===> Example: Destructuring with default values
+const testArrayTwo = ["First string", "Second string"];
+let [firstString, secondString, thirdString = "Third string :)"] = testArrayTwo;
+console.log(firstString, secondString, thirdString);
+
+// ===> Example: Swapping variables using array destructuring
+// let a = 10;
+// let b = 20;
+let a = 10,
+  b = 20; // same thing as above
+// HOW TO SWAP THEIR VALUES ???
+// => Without Destructuring
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+// => With Destructuring
+[a, b] = [b, a];
+console.log(a, b);
+
+console.clear();
+console.log("");
+console.log("============== Spread operator ==============");
+// The spread operator (...) is used to expand iterable elements (arrays, objects, strings) into individual elements or properties
+// It is commonly used for copying, merging and passing elements as arguments to functions
+
+// *** Spread in fucntion calls ***
+
+const numbers = [-5, 10, 30, 123, 10_000];
+console.log(Math.max(numbers));
+// console.log(Math.max(-5, 10, 30, 123, 10_000));
+console.log(Math.max(...numbers));
+
+console.log(numbers);
+console.log(...numbers);
+
+// *** Spread with Arrays ***
+
+const dogs = ["Bax", "Chapo", "Sharko"];
+const cats = ["Zuza", "Mici"];
+
+// ===> Example: merge 2 arrays into 1 (WITHOUT SPREAD)
+let allPetsArr = [];
+for (const pet of dogs) {
+  allPetsArr.push(pet);
+}
+for (const pet of cats) {
+  allPetsArr.push(pet);
+}
+
+// ===> Example: merge 2 arrays into 1 (WITH SPREAD)
+const allPets = [...dogs, ...cats];
+console.log(allPets);
+
+// ===> Example: create copy of dogs array
+const dogsCopy = [...dogs];
+dogsCopy.push("Pluto"); // won't change the original dogs array
+
+// *** Spread with Objects ***
+
+// ===> Example: merge 3 objects into 1 (WITH SPREAD)
+
+let dog = {
+  name: "Aks",
+  breed: "Pug",
+};
+
+let description = {
+  group: "Toy",
+  color: "Appricot Fawn",
+  origin: "China",
+};
+
+let owner = {
+  firstName: "Bob",
+};
+
+const aksDogInfo = { ...dog, ...description, ...owner }; //, isHappy: true } // can add extra properties if needed
+console.log(aksDogInfo);
+
+// *** Spread with Strings ***
+// ===> Example: Converts a string into an array of characters
+const word = "HELLO";
+const letters = [...word];
+console.log(letters);
+
+console.log(...aksDogInfo.name);
+
+console.log("");
+console.log("============== Rest operator ==============");
+
+// ===> Example: take first 2 elements from array. Assign the rest as an array to a variable.
+const students = ["Bob", "Jill", "John", "Rob", "Jo"];
+
+const [bobName, jillName, ...restStudents] = students;
+console.log(bobName, jillName);
+console.log(restStudents);
+
+// ===> Example: function that sums random count of numbers passed as arguments
+
+// function sum(num1, num2, num3) {
+//     // console.log(arguments);
+//     return num1 + num2 + num3;
+// }
+// console.log(sum(1, 2, 3, 4));
+
+function sum(...numbers) {
+  let result = 0;
+  for (const number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+console.log(sum(10, 20, 30, 40, 50));
+console.log(10, 20, 30, 40, 50);
