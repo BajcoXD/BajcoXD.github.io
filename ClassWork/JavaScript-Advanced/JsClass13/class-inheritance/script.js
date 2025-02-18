0885a4447fc5cf3ac773709dece615404991a52e
Code <code@sedc.mk>
2025-02-18 18:29:55 +0100

    test1

--- ClassWork/JavaScript-Advanced/JsClass13/class-inheritance/script.js
+++ ClassWork/JavaScript-Advanced/JsClass13/class-inheritance/script.js
@@ -31,7 +31,102 @@ class Employee {
 const bob = new Employee("Bob", "Bobsky", 30, 3000);
 console.log(bob);
 bob.printInfo();
+console.log("=========================================");
 
 class Developer extends Employee {
-  constructor(firstName, lastName, age, salary, programingLanguages) {}
+  constructor(firstName, lastName, age, salary, programingLanguages) {
+    super(firstName, lastName, age, salary);
+    this.programingLanguages = programingLanguages;
+  }
+  code() {
+    console.log(`${this.firstName} is doing magic :D`);
+  }
 }
+class Tester extends Employee {
+  constructor(firstName, lastName, age, salary, bugsFound) {
+    super(firstName, lastName, age, salary);
+    this.bugsFound = bugsFound;
+  }
+  testSoftware() {
+    console.log(
+      `${this.firstName} is testing the software and found ${this.bugsFound}`
+    );
+  }
+}
+class Menager extends Employee {
+  constructor(firstName, lastName, age, salary, department) {
+    super(firstName, lastName, age, salary);
+    this.department = department;
+  }
+  organizeMeeting() {
+    console.log(
+      `The Menager ${this.firstName} is orginazing a meeting for ${this.department} department`
+    );
+  }
+}
+class AutomationTester extends Tester {
+  constructor(
+    firstName,
+    lastName,
+    age,
+    salary,
+    bugsFound,
+    automationFramework
+  ) {
+    super(firstName, lastName, age, salary, bugsFound);
+    this.automationFramework = automationFramework;
+  }
+  writeAutomationFramework() {
+    console.log(
+      `${this.firstName} is writing Automation Tests using the ${this.automationFramework} framework.`
+    );
+  }
+}
+const dev = new Developer("Viktor", "Stojanovski", 19, 1000, [
+  "JavaScript",
+  "CSS",
+  "HTML",
+]);
+dev.printInfo();
+dev.code();
+console.log("=========================================");
+
+const tester = new Tester("Boban", "Bobski", 19, 2000, 5);
+tester.printInfo();
+tester.testSoftware();
+console.log("=======================================");
+
+const menager = new Menager("Palco", "Palcovski", 30, 5000, "QinShift");
+tester.printInfo();
+tester.testSoftware();
+console.log("=====================================");
+
+const automationTester = new AutomationTester(
+  "Test",
+  "Testovski",
+  25,
+  2400,
+  5,
+  "Selenium"
+);
+automationTester.printInfo();
+automationTester.testSoftware();
+automationTester.writeAutomationFramework();
+
+console.log(automationTester.__proto__);
+console.log(automationTester.__proto__.__proto__);
+console.log(automationTester.__proto__.__proto__.__proto__);
+
+console.log("");
+console.log(
+  "=========== Check if an object is an instance of a class ==========="
+);
+console.log(dev instanceof Developer); // true
+console.log(dev instanceof Employee); // true
+console.log(dev instanceof Tester); // flase
+
+console.log("============================================");
+
+console.log(automationTester instanceof Tester); // true
+console.log(automationTester instanceof AutomationTester); // true
+console.log(automationTester instanceof Employee); // true
