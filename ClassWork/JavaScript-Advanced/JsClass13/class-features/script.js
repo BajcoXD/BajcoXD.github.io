1b13b8dcaa2ee4eb16cf5eb068a09ba22ccbe9f1
Code <code@sedc.mk>
2025-02-18 20:17:15 +0100

    Class Features

--- ClassWork/JavaScript-Advanced/JsClass13/class-features/script.js
+++ ClassWork/JavaScript-Advanced/JsClass13/class-features/script.js
@@ -1,7 +1,10 @@
-console.log("================== STATIC MEMBERS ====================");
+console.log("=================== STATIC MEMBERS ===================");
+
 class Employee {
   static idCounter = 0;
+
   constructor(firstName, lastName, age, salary) {
+    // this.id = ++this.idCounter; // ERROR
     this.id = ++Employee.idCounter;
     this.firstName = firstName;
     this.lastName = lastName;
@@ -10,41 +13,117 @@ class Employee {
   }
 
   printInfo() {
+    // console.log(`${this.idCounter}. Employee: ${this.firstName} ${this.lastName}. Age ${this.age}. Salary ${this.salary}.`);
     console.log(
-      `${this.idCounter}. Employee: ${this.firstName} ${this.lastName}. Age ${this.age}. Salary ${this.salary}.`
+      `Employee: ${this.firstName} ${this.lastName}. Age ${this.age}. Salary ${this.salary}.`
     );
   }
+
   greetColleague(name) {
     console.log(`${this.firstName} says hello to ${name}`);
   }
+
   static getTotalEmployees() {
-    return `Total Employes ${Employee.idCounter}`;
+    return `Total Employees: ${Employee.idCounter}`;
   }
 }
-const bob = new Employee("Bob", "Bobski", 20, 2000);
+
+const bob = new Employee("Bob", "Bobsky", 35, 3000);
 bob.printInfo();
 console.log(bob.id);
 console.log(bob);
-console.log(bob.idCounter);
+
+console.log(bob.idCounter); // undefined
 console.log(Employee.idCounter);
 
-const getTotalEmployees = Employee.getTotalEmployees();
-console.log(getTotalEmployees);
+// bob.getTotalEmployees(); // ERROR
+const employee1 = new Employee();
+const employee2 = new Employee();
+const employee3 = new Employee();
+
+const totalEmployees = Employee.getTotalEmployees();
+console.log(totalEmployees); // 4
+
+// ===> Example: StringHelper - A Utility Class for Common String Operations
 
 class StringHelper {
   static toTitleCase(str = "") {
-    let splitedString = str.split(" ");
-    for (let i = 0; i < splitedString.length; i++) {
-      let word = splitedString[i];
-      splitedString[i] =
+    let splitString = str.split(" ");
+    for (let i = 0; i < splitString.length; i++) {
+      let word = splitString[i];
+      splitString[i] =
         word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     }
-    return splitedString.join(" ");
+    return splitString.join(" ");
   }
-  static truncate(str, maxLenght) {
-    return str.length < maxLenght ? str : str.substring(0, maxLenght) + "...";
+
+  static truncate(str, maxLength) {
+    return str.length < maxLength ? str : str.substring(0, maxLength) + "...";
+  }
+
+  // more string operations ...
+}
+
+console.log(StringHelper.toTitleCase("tHis iS MeSSEd uP teXT"));
+// tHis iS MeSSEd uP teXT => This Is Messed Up Text
+
+console.log(
+  StringHelper.truncate(
+    "This is very very very very very very very very very long string",
+    7
+  )
+);
+// This is very very very very very very very very very long string => This is...
+
+console.log("");
+console.log("============== GET / SET ==============");
+
+class Product {
+  static idCounter = 0;
+
+  constructor(name, price) {
+    this.id = ++Product.idCounter;
+    this.name = name;
+    this.price = price;
+  }
+
+  get price() {
+    console.log("HELLO FROM PRICE PROPERTY GETTER");
+    // return this.price;
+    return `$${this._price.toFixed(2)}`;
+  }
+
+  set price(value) {
+    console.log("HELLO FROM PRICE PROPERTY SETTER");
+    if (value <= 0) {
+      console.error("Price cannot be negative or zero !");
+      return;
+    }
+    this._price = value;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(value) {
+    if (!this.id) {
+      console.log(`Id ${value} added`);
+      this._id = value;
+    } else {
+      console.error("Cannot reassign id value !");
+    }
   }
 }
 
-console.log(StringHelper.toTitleCase("ThiS IS mesSed UP TeXT"));
-console.log(StringHelper.truncate("This is very very very very very long string", 7));
+const product = new Product("Laptop", 1000);
+// console.log(product.name);
+// console.log(product.price); // 1000 => $1000.00
+
+product.price = -100;
+product.price = 22_222;
+// console.log(product.price);
+product.name = "New Product";
+// console.log(product.name);
+product.id = 1000;
+console.log(product);
