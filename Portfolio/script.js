7b22c43d59e571c528023afb038b87c2e85a2000
BajcoXD <viktor.gaming2018@gmail.com>
2025-10-09 18:15:17 +0200

    Test 2

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -11,8 +11,41 @@ function cancel() {
 }
 
 // optional: close dropdown when clicking a link (good UX)
-document.querySelectorAll(".dropdown .links a").forEach(link => {
+document.querySelectorAll(".dropdown .links a").forEach((link) => {
   link.addEventListener("click", () => {
     document.querySelector(".dropdown").classList.remove("open");
   });
 });
+
+const texts = ["FRONT END", "BACK END", "FULL STACK WEB DEVELOPER"];
+const speed = 100;
+const eraseSpeed = 50;
+const delayBetween = 1500; // wait before erasing/typing again
+
+const textElement = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charIndex = 0;
+
+function typeWriter() {
+  if (charIndex < texts[textIndex].length) {
+    textElement.innerHTML += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, delayBetween);
+  }
+}
+
+function eraseText() {
+  if (charIndex > 0) {
+    textElement.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseText, eraseSpeed);
+  } else {
+    textIndex = (textIndex + 1) % texts.length; // go to next word
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter;
